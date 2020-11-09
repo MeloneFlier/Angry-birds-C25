d49b98963b4ffb8952eacc3f37ceffba381ec16b
Master-glicth <72987026+Master-glicth@users.noreply.github.com>
2020-11-09 20:06:48 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,11 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height) {
+   super (x,y,width,height);
+   this.image = loadImage("sprites/wood1.png");
+  }
+  display(){
+
+super.display();
+
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Angry-birds-C25
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
